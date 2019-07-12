b9882de17684aa6df70809ddaf3bfe64f8939bcf
kat3005 <kasia.alicja@gmail.com>
2019-07-12 12:41:51 +0100

    Adds classes

---
+++ models/customers.rb
@@ -0,0 +1,14 @@
+require_relative("../db/sql_runner")
+
+class Customers
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  end
