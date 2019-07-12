6a078d7b25d08f07040dcffc5ac6edb9b870069c
kat3005 <kasia.alicja@gmail.com>
2019-07-12 15:17:55 +0100

    Adds delete method to ticket

--- models/ticket.rb
+++ models/ticket.rb
@@ -26,4 +26,15 @@ class Ticket
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE * FROM tickets where id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM films"
+    SqlRunner.run(sql)
+  end
+
 end
