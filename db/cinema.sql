cd9e43d0257ebf9af3e53d084cbae32d6f2df0cf
kat3005 <kasia.alicja@gmail.com>
2019-07-14 17:33:49 +0100

    Adds screenings table to sql file

--- db/cinema.sql
+++ db/cinema.sql
@@ -1,11 +1,18 @@
 DROP TABLE tickets;
 DROP TABLE films;
+DROP TABLE screenings;
 DROP TABLE customers;
 
 CREATE TABLE films(
   id SERIAL4 PRIMARY KEY,
   title VARCHAR(255),
   price INT4
+);
+
+CREATE TABLE screenings(
+  id SERIAL4 PRIMARY KEY,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  screening_time VARCHAR(255)
 );
 
 CREATE TABLE customers(
@@ -16,7 +23,7 @@ CREATE TABLE customers(
 
 CREATE TABLE tickets(
   id SERIAL4 PRIMARY KEY,
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  screening_id INT4 REFERENCES screenings(id) ON DELETE CASCADE,
   customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE
 
 );
